e81222310dd8a05316d87885c03d654276009cb7
Qiusheng Wu <giswqs@gmail.com>
2026-02-07 00:31:35 -0500

    fix: add missing CSS imports to COG and Zarr examples

--- examples/zarr-layer/main.ts
+++ examples/zarr-layer/main.ts
@@ -1,5 +1,8 @@
 import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import { ZarrLayerControl, ZarrLayerAdapter } from '../../src';
+import '../../src/lib/styles/zarr-layer.css';
+import '../../src/lib/styles/common.css';
 import { LayerControl } from 'maplibre-gl-layer-control';
 import 'maplibre-gl-layer-control/style.css';
 
