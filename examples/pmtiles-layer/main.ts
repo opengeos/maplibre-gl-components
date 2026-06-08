fe16ba0b60ecf3d4d91b764122c7e4ae95ea97a3
Daniel <danieldoh.inbox@gmail.com>
2026-06-08 11:20:17 -0500

    added first static layer

--- examples/pmtiles-layer/main.ts
+++ examples/pmtiles-layer/main.ts
@@ -29,6 +29,8 @@ const pmtilesControl = new PMTilesLayerControl({
   loadDefaultUrl: true,
 });
 
+// await pmtilesControl.addLayer('layers\intermixed.pmtiles');
+await pmtilesControl.addLayer('/layers/intermixed.pmtiles');
 
 // Create an adapter to integrate PMTiles layers with the layer control
 const pmtilesAdapter = new PMTilesLayerAdapter(pmtilesControl, {
